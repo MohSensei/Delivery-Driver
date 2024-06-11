f7f98abd565dc7843832f4efdd0b88365cf1c047
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-06-11 01:53:26 -0400

    Added a Collision class

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float moveSpeed = 5f;
     void Start()
     {
 
@@ -13,7 +14,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, .01f, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
