deb63522636c2f691d2f64115d77223efdc9e924
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-07-31 16:09:27 -0400

    Added a speed boost trigger, whent he player triggers the speed boost the speed of the player will increase. If the player happened to hit something his speed will decreas

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 200f;
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 25f;
     void Start()
     {
 
@@ -18,5 +20,17 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
