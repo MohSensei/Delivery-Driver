95e79618c709856c7f8c775e8d51058b0e7384fc
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-07-31 13:20:29 -0400

    Added the destroy function to the Pacakges, so when the player collids with a package the package will be gone

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-
+    [SerializeField]
+    float destroyDelay = 0.5f;
     bool hasPackage;
 
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "customer" && hasPackage)
         {
             Debug.Log("Package is Delivered");
+            hasPackage = false;
         }
     }
 
